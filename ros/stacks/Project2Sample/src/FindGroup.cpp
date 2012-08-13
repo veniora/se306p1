700ef76e274af796e4399c32e01ef78842b8c18f
msan065 <marypauline.santos@gmail.com>
2012-08-13 20:11:49 +1200

    Francis+Pauline calculatePosition method in FindGroup class

--- ros/stacks/Project2Sample/src/FindGroup.cpp
+++ ros/stacks/Project2Sample/src/FindGroup.cpp
@@ -6,6 +6,10 @@
  */
 
 #include "FindGroup.h"
+#include <vector>
+
+#define PI 3.14159265
+
 using namespace std;
  //checks distance
 bool sortByDistance (Project2Sample::R_ID robot1, Project2Sample::R_ID robot2) {
@@ -24,6 +28,7 @@ FindGroup::~FindGroup() {
 }
 
 class FindGroup {
+
 public:
 	static vector<int> formGroup(vector<Project2Sample::R_ID> nodes, int robotID) {
 		vector<int> robotGroupInfo;
@@ -32,16 +37,57 @@ public:
 		//number of leaders
 		int numOfLeaders = nodes.size() / 6;
 		sort(nodes.begin(), nodes.end(), sortByDistance);
+		if (fabs(nodes.at(0).x) < 0.0001 && fabs(nodes.at(0).y) < 0.0001) {
+			nodes.insert(nodes.begin()+numOfLeaders+1, nodes.at(0));
+			nodes.erase(nodes.begin());
+		}
 		for (i = 0; i < nodes.size(); ++i) {
 			if (nodes.at(i).R_ID == robotID) {
 				groupID = i % numOfLeaders;
 				posID = i / numOfLeaders;
-				robotGroupInfo.push_back(groupID);
+				//robotGroupInfo.push_back(groupID);
+				robotGroupInfo.push_back(nodes.at(groupID).R_ID);
 				robotGroupInfo.push_back(posID);
 				break;
 			}
 		}
-		return robotGroupInfo;
+		return robotGroupInfo;      //[LeaderID, posID]
+	}
+	//only need values for leader
+	static vector<float> calculatePosition(Project2Sample::R_ID leader, int posID) {
+		vector<float> newCoordinates;
+		float fiveRobots = 5 * 0.35; // Assuming robot length of 0.35
+		float newX, newY;
+		//calculate the angle the robot position vector makes with the positive x axis
+		// Angles in radians
+		float lineAngle = atan(leader.y/leader.x);
+		lineAngle = lineAngle * 180 / PI;
+		float leaderTheta = 180 + lineAngle;
+
+		// new value of x and y for the robot in the posID
+
+		int check = 2(leader.x/fabs(leader.x)) + (leader.y/fabs(leader.y));
+
+		switch (check) {
+			case 3: newX = (fiveRobots * cos(lineAngle)* posID) + leader.x;
+					newY = (fiveRobots * sin(lineAngle)* posID) + leader.y;
+					break;
+			case 1: newX = (fiveRobots * cos(lineAngle)* posID) + leader.x;
+					newY = -(fiveRobots * sin(lineAngle)* posID) + leader.y;
+					break;
+			case -1: newX = -(fiveRobots * cos(lineAngle)* posID) + leader.x;
+					 newY = (fiveRobots * sin(lineAngle)* posID) + leader.y;
+					 break;
+			case -3: newX = -(fiveRobots * cos(lineAngle)* posID) + leader.x;
+			         newY = -(fiveRobots * sin(lineAngle)* posID) + leader.y;
+			         break;
+		}
+		newCoordinates.push_back(newX);
+		newCoordinates.push_back(newY);
+		newCoordinates.push_back(leaderTheta);
+		//vector [newX, newY, leadeTheta]
+		return newCoordinates;
+
 	}
 };
 
--- ros/stacks/Project2Sample/src/FindGroup.h
+++ ros/stacks/Project2Sample/src/FindGroup.h
@@ -16,7 +16,7 @@ public:
 	virtual ~FindGroup();
 	static vector<int> formGroup(vector<Project2Sample::R_ID> nodes, int robotID);
 	bool sortByDistance (int i,int j);
-
+	static vector<float> calculatePosition(Project2Sample::R_ID leader, int posID);
 };
 
 #endif /* FINDGROUP_H_ */

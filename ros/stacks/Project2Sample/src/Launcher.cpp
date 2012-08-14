c614adc23b92135169112da7b97e7ce9e08ab0ca
brettyukich <brettyukich@gmail.com>
2012-08-15 11:46:19 +1200

    cleaned up launcher a bit

--- ros/stacks/Project2Sample/src/Launcher.cpp
+++ ros/stacks/Project2Sample/src/Launcher.cpp
@@ -1,58 +1,59 @@
-#include <stdlib.h>
-#include <unistd.h>
-#include <iostream>
+#include <stdlib.h> // atoi
+#include <iostream> // cout
 #include <sstream>
-
-// basic file operations
-#include <iostream>
-#include <fstream>
 
 using namespace std;
 
 /**
- * This is a factory for creating RX robots. It simply starts x number of robots thens finishes
+ * This program starts roscore, stage (with x many robots) and x many RX nodes. To achieve this it uses fork() and execl().
+ * roscore and stage are started by running roslaunch ... .
+ * The RX nodes are then started separate from roslaunch.
  */
 int main(int argc, char **argv) {
 	int i;
 	int pid;
-	if (argc != 2){
-		cout << "To run launcher use ./Launcher X, where X is the number of robots to start\n";
+	if (argc != 2) {
+		cout
+				<< "To run launcher use ./Launcher X, where X is the number of robots to start\n";
 		return 0;
 	}
 	stringstream ss;
 	ss << "numbots:=" << argv[1];
 	// start roscore + stage
 	pid = fork();
-	if (pid == 0){
-		// roscore is not garunteed to be running before the nodes are created
-		execl("/opt/ros/electric/ros/bin/roslaunch", "/opt/ros/electric/ros/bin/roslaunch",
-				"Project2Sample", "alphaX.launch", ss.str().c_str(), NULL);
+	if (pid == 0) {
+		// roscore is not guaranteed to be running before the nodes are created
+		execl("/opt/ros/electric/ros/bin/roslaunch",
+				"/opt/ros/electric/ros/bin/roslaunch", "Project2Sample",
+				"alphaX.launch", ss.str().c_str(), NULL);
+		// If excel succeeds then these two lines wont ever be reached
 		cout << "roscore failed to start";
 		return 0;
 	}
 
-
 	// start robots
 	for (i = 0; i < atoi(argv[1]); ++i) {
 		pid = fork();
-		cout << "fork:" << i ;
+		cout << "fork:" << i;
 
-		if (pid == 0){
+		if (pid == 0) {
 			// this is new process
 			stringstream ss;
 			ss << i;
 			// execl replace this process
-			execl("RX", "RX", ss.str().c_str(), NULL);
+			execl("RX", "RX", ss.str().c_str(), NULL); //RX must be in the same location as Launcher
 			// if execl executes correctly then the cout will never happen
-			cout << "process:" << i ;
+			cout << "process:" << i;
 			break;
-		} else if (pid == -1){
-			// error
+		} else if (pid == -1) {
+			// An error has occurred
 		} else {
 			// parent process so continue looping
 		}
 	}
-	while (true){} // If this process finishes then all the fork/exec'd process become hard to kill
+	while (true) {
+		// If this process finishes then all the fork/exec'd process become hard to kill
+	}
 	return 0;
 
 }

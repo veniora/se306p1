fa174764df595ccb701be7a92d6f87689ff62210
Michael Ford <veniora7@gmail.com>
2012-08-15 21:29:11 +1200

    Further changes

--- ros/stacks/Project2Sample/test/TestFindGroup.cpp
+++ ros/stacks/Project2Sample/test/TestFindGroup.cpp
@@ -8,7 +8,9 @@
 #include "TestFindGroup.h"
 #include "ros/ros.h"
 #include <gtest/gtest.h>
+#include <Project2Sample/R_ID.h>
 
+using namespace std;
 TestFindGroup::TestFindGroup() {
 	// TODO Auto-generated constructor stub
 
@@ -21,6 +23,34 @@ TestFindGroup::~TestFindGroup() {
 TEST(TestFindGroup, Initial)
 {
 	EXPECT_EQ(10, 10 );
+}
+
+TEST(TestFindGroup, simpleOneGroupCase)
+{
+	// Define 6 robots - vectors are R_IDs with 5 lines: int id, int group id, float x, float y, float theta
+	vector<Project2Sample::R_ID> nodes;
+	Project2Sample::R_ID msg1;
+	msg1.R_ID = 0; msg1.x = 10.0; msg1.y = 10.0;
+	nodes.push_back(msg1);
+	Project2Sample::R_ID msg2;
+	msg2.R_ID = 1; msg2.x = 20.0; msg2.y = 20.0;
+	nodes.push_back(msg2);
+	Project2Sample::R_ID msg3;
+	msg3.R_ID = 2; msg3.x = 30.0; msg3.y = 30.0;
+	nodes.push_back(msg3);
+
+	vector<int> robotGroupInfo = FindGroup::formGroup(nodes, 1);
+
+	EXPECT_EQ(0, robotGroupInfo.at(0));
+	EXPECT_EQ(0, robotGroupInfo.at(1));
+	EXPECT_EQ(1, robotGroupInfo.at(2));
+	cout << "formGroupTest FOUND!" < "\n";
+
+	//delete robotGroupInfo, msg1, msg2, msg3;
+
+
+
+//<test things here, calling EXPECT_* and/or ASSERT_* macros as needed>
 }
 
 // Run all the tests that were declared with TEST()

dee32bce7d6c4c5a520fc1dc61b18e6b1392564e
brettyukich <brettyukich@gmail.com>
2012-08-17 15:51:25 +1200

    added missing *_test.cpp files & removed R0

---
+++ ros/stacks/Project2Sample/test/GetGroup_test.cpp
@@ -0,0 +1,22 @@
+/*
+ * Example_test.cpp
+ *
+ *  Created on: 17/08/2012
+ *      Author: Brett
+ */
+#include <gtest/gtest.h>
+#include <ros/ros.h>
+#include "../src/GetGroup.h"
+
+TEST(Example_tests, test1) {
+	// Assert stuff here
+}
+
+TEST(Example_tests, test2) {
+
+}
+
+int main(int argc, char **argv) {
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}

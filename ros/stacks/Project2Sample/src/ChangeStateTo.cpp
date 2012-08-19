418be1fa553cef44dffe895a03a1347ccfba30f2
brettyukich <brettyukich@gmail.com>
2012-08-19 12:27:47 +1200

    minor fixes

--- ros/stacks/Project2Sample/src/ChangeStateTo.cpp
+++ ros/stacks/Project2Sample/src/ChangeStateTo.cpp
@@ -1,9 +1,9 @@
 #include <ros/ros.h>
 #include "Project2Sample/State.h"
 /*
- * PublishState.cpp
- *
- *  Created on: Aug 14, 2012
+ * ChangeToState.cpp
+ * This program publishes a single message to the RobotState topic.
+ * Created on: Aug 14, 2012
  *      Author: charu
  */
 
@@ -13,12 +13,13 @@ int main(int argc, char **argv) {
 	int newState;
 	// Check input arguments
 	if (argc != 2) {
-		cout << "To run ChangeStateTo use ./ChangeStateTo X, where X is the value of the state to change to.\n";
+		cout
+				<< "To run ChangeStateTo use ./ChangeStateTo X, where X is the value of the state to change to.\n";
 		cout << "State values: \n";
 		cout << "IDLE = 0 \n";
 		cout << "FORMING_GROUP = 1\n";
 		cout << "MOVING_INTO_POS = 2\n";
-		cout <<	"FETCH_INSTRUCTIONS = 3\n";
+		cout << "FETCH_INSTRUCTIONS = 3\n";
 		cout << "CIRCLING = 4\n";
 		cout << "FORM_SQUARE = 5\n";
 		cout << "FORM_CIRCLE = 6\n";
@@ -38,6 +39,7 @@ int main(int argc, char **argv) {
 	Project2Sample::State robot_state;
 	robot_state.state = newState;
 	state_publisher.publish(robot_state);
+	ros::spinOnce();
 
 	sleep(1);
 

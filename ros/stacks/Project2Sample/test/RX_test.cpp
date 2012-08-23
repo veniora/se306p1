20d48256671ab37f7a91ad3211854da43898c866
brettyukich <brettyukich@gmail.com>
2012-08-23 22:39:53 +1200

    added a new intergration test

--- ros/stacks/Project2Sample/test/RX_test.cpp
+++ ros/stacks/Project2Sample/test/RX_test.cpp
@@ -4,7 +4,13 @@
 #include <Project2Sample/State.h>
 #include <nav_msgs/Odometry.h>
 
+// testStateChange variables
 int state;
+
+// testTruth variables
+double x;
+double y;
+double z;
 
 /**
  * Change the global variable 'state' to the state of the RX node.
@@ -13,15 +19,23 @@ void test_callback(Project2Sample::R_ID msg) {
 	state = msg.R_State;
 }
 
+void testTruth_callback(Project2Sample::R_ID msg){
+	x = msg.x;
+	y = msg.y;
+	z = msg.theta;
+
+}
+
 /**
  * Test that a RX node responds correctly to a State message published to the 'Robot_state' topic
  */
-TEST(TestSuite, testStateChange) {
+TEST(IntergrationTests, testStateChange) {
 
 	state = -1;
 	ros::NodeHandle node_handel;
 	ros::Publisher odom_publisher = node_handel.advertise<nav_msgs::Odometry>("Robot1_truth", 1000);
 	ros::Publisher state_publisher = node_handel.advertise<Project2Sample::State>("Robot_state", 1000);
+	ros::Subscriber RobotNode_sub = node_handel.subscribe<Project2Sample::R_ID>("Robot_msg", 1000, test_callback);
 	ros::Rate loop_rate(10);
 
 	// RX wait for and odometry message before it can do anything else so we must send it a dummy one
@@ -49,9 +63,6 @@ TEST(TestSuite, testStateChange) {
 	robot_state.group = -1;
 	state_publisher.publish(robot_state);
 
-	ros::Subscriber RobotNode_sub = node_handel.subscribe<Project2Sample::R_ID>(
-			"Robot_msg", 1000, test_callback);
-
 	// Sleep for one second to ensure the RX node has time to process and publish.
 	sleep(1);
 
@@ -59,6 +70,43 @@ TEST(TestSuite, testStateChange) {
 	ros::spinOnce();
 
 	EXPECT_EQ(1, state);
+}
+
+/**
+ * Test that a RX node responds correctly to a Odometry message published to the 'RobotX_truth' topic
+ */
+TEST(IntergrationTests, testTruth){
+	ros::NodeHandle node_handel;
+	ros::Publisher odom_publisher = node_handel.advertise<nav_msgs::Odometry>("Robot2_truth", 1000);
+	ros::Subscriber RobotNode_sub = node_handel.subscribe<Project2Sample::R_ID>(
+					"Robot_msg", 1000, testTruth_callback);
+
+
+	// wait until RX subscribes to the topic
+	while (odom_publisher.getNumSubscribers() == 0) {
+		ros::Rate(10).sleep();
+	}
+
+	// create a dummy odom message
+	nav_msgs::Odometry odom_message;
+	odom_message.pose.pose.position.x = 5.0;
+	odom_message.pose.pose.position.y = 10.0;
+	odom_message.pose.pose.position.z = 1.0; // position.z represents theta radians
+
+	// publish
+	odom_publisher.publish(odom_message);
+
+
+	// Give the node time to process
+	sleep(2);
+
+	ros::spinOnce();
+
+	EXPECT_EQ((double)5.0, x);
+	EXPECT_EQ((double)10.0, y);
+	EXPECT_NEAR(double(147.295779513), z, (double)0.0001);
+
+	// 147.295779513
 }
 
 // Run all the tests that were declared with TEST()
